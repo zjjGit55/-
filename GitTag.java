995af1baacf8a3b4db6c71fb78b9a108da58ef9b
yinghong-Lin <2687138072@qq.com>
2024-09-08 13:41:26 +0800

    SECOND COMMIT

--- GitTag.java
+++ GitTag.java
@@ -3,7 +3,9 @@ package test;
 public class GitTag {
 	public static void main(String[] args) {
 		System.out.println("Version1.0");
-		System.out.println("张珈嘉");
+		System.out.println("张珈嘉");	
+		System.out.println("林莹虹");
+
 	}
 }
 
